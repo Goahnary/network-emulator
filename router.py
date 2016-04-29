8efa553a5a620f6df9ff538fcc24434e21a7ef50
Daniel Salami <danialskywalker45@yahoo.com>
2016-04-28 21:41:26 -0500

    work done by daniel

--- router.py
+++ router.py
@@ -6,7 +6,8 @@ import select
 import random
 import json
 
-class router(object):
+
+class router:
 	
 	"""docstring for router"""
 
@@ -20,39 +21,100 @@ class router(object):
 
 	try :
 		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	    print 'Socket created'
-	except socket.error, msg :
-	    print 'Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-	    sys.exit()
 
-	# try:
-	#     s.bind((IP, PORT))
-	    
-	# except socket.error , msg:
-	#     print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-	#     sys.exit()
+	except socket.error, msg :
+		print 'Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+		sys.exit()
+
+# try:
+#     s.bind((IP, PORT))
+
+# except socket.error , msg:
+#     print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+#     sys.exit()
 
 
 	def __init__(self, rn):
 		self.routerNum = rn
 
-
 	def add_neighbor(self, address): 	# address = (IP, Port)
 		n = add_weight(address)			# returns a 
-
 		self.neighbors.append(n)
 
-	def add_weight(self, n): # Adds weight between this router and a neighbor 
-		weight = random.randrange(1,11)
-		return (n, weight)
+	def add_weight(self): # Adds weight between this router and a neighbor 
+		self.weight = random.randint(1,11)
+		
 
-	def get_address(self): # Gives router IP and Port
-		self.IP = "localhost"
-		self.PORT = BASE_PORT + (MULTIPLIER * self.routerNum)
+	def get_address(self, IP, PORT): # Gives router IP and Port
+		self.IP = IP
+		self.PORT = PORT
 
 	def get_neighbors(self): # Returns list of adjacent routers
 		return neighbors
 
-	
+	def getIP(self):
+		self.IP = "localhost"
+
 	def update_table(self, json_table): #Updates the forwarding table to the current one
 		self.table = json.loads(json_table)
+
+	def connectNeighbour(self, neighbor):
+		try :
+			self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		except socket.error, msg :
+			print 'Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+			sys.exit()
+
+		try:
+			self.socket.bind((neighbor.IP, neighbor.PORT))
+			print 'Successfully connected to neighbor.............................'
+		except socket.error, msg :
+			print 'Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+			sys.exit()
+
+
+print "hi"
+router1 = router(1)
+router2 = router(2)
+router3 = router(3)
+router4 = router(4)
+router5 = router(5)
+
+print "hello router " + str(router1.routerNum)
+print "hello router " + str(router2.routerNum)
+print "hello router " + str(router3.routerNum)
+print "hello router " + str(router4.routerNum)
+print "hello router " + str(router5.routerNum)
+
+router1.get_address("localhost", 7777)
+router2.get_address("localhost", 1080)
+router3.get_address("localhost", 1098)
+router4.get_address("localhost", 1140)
+router5.get_address("localhost", 1214)
+
+print router1.IP
+print router1.PORT
+print router2.IP
+print router2.PORT
+print router3.IP
+print router3.PORT
+print router4.IP
+print router4.PORT
+print router5.IP
+print router5.PORT
+
+router1.add_weight()
+router2.add_weight()
+router3.add_weight()
+router4.add_weight()
+router5.add_weight()
+
+router1.connectNeighbour(router2)
+router2.connectNeighbour(router5)
+router3.connectNeighbour(router4)
+
+print router1.weight
+print router2.weight
+print router3.weight
+print router4.weight
+print router5.weight
