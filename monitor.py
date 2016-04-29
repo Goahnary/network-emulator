89cff70d9e57d87e9ca318ad582bdc50580f46ba
Noah Gary <goahnary@gmail.com>
2016-04-28 22:21:56 -0500

    monitor working now

--- monitor.py
+++ monitor.py
@@ -1,7 +1,19 @@
-import router
+from router import Router 
 
-r1 = router(1)
-r2 = router(2)
+print "Ello Mate"
+
+r1 = Router(1)
+r2 = Router(2)
+r3 = Router(3)
+r4 = Router(4)
+r5 = Router(5)
+r6 = Router(6)
+r7 = Router(7)
+r8 = Router(8)
+r9 = Router(9)
+r10 = Router(10)
+
+
 
 r1.add_neighbor(r2)
 r2.add_neighbor(r1)
--- router.py
+++ router.py
@@ -7,7 +7,7 @@ import random
 import json
 
 
-class router:
+class Router:
 	
 	"""docstring for router"""
 
@@ -36,24 +36,21 @@ class router:
 
 	def __init__(self, rn):
 		self.routerNum = rn
+		get_address()
 
 	def add_neighbor(self, address): 	# address = (IP, Port)
 		n = add_weight(address)			# returns a 
 		self.neighbors.append(n)
 
 	def add_weight(self): # Adds weight between this router and a neighbor 
-		self.weight = random.randint(1,11)
-		
+		self.weight = random.randint(1,11)		
 
-	def get_address(self, IP, PORT): # Gives router IP and Port
-		self.IP = IP
-		self.PORT = PORT
+	def get_address(self, PORT): # Gives router IP and Port
+		self.IP = "localhost"
+		self.PORT = BASE_PORT + self.routerNum*7
 
 	def get_neighbors(self): # Returns list of adjacent routers
 		return neighbors
-
-	def getIP(self):
-		self.IP = "localhost"
 
 	def update_table(self, json_table): #Updates the forwarding table to the current one
 		self.table = json.loads(json_table)
@@ -73,48 +70,48 @@ class router:
 			sys.exit()
 
 
-print "hi"
-router1 = router(1)
-router2 = router(2)
-router3 = router(3)
-router4 = router(4)
-router5 = router(5)
+# print "hi"
+# router1 = Router(1)
+# router2 = Router(2)
+# router3 = Router(3)
+# router4 = Router(4)
+# router5 = Router(5)
 
-print "hello router " + str(router1.routerNum)
-print "hello router " + str(router2.routerNum)
-print "hello router " + str(router3.routerNum)
-print "hello router " + str(router4.routerNum)
-print "hello router " + str(router5.routerNum)
+# print "hello router " + str(router1.routerNum)
+# print "hello router " + str(router2.routerNum)
+# print "hello router " + str(router3.routerNum)
+# print "hello router " + str(router4.routerNum)
+# print "hello router " + str(router5.routerNum)
 
-router1.get_address("localhost", 7777)
-router2.get_address("localhost", 1080)
-router3.get_address("localhost", 1098)
-router4.get_address("localhost", 1140)
-router5.get_address("localhost", 1214)
+# router1.get_address("localhost", 7777)
+# router2.get_address("localhost", 1080)
+# router3.get_address("localhost", 1098)
+# router4.get_address("localhost", 1140)
+# router5.get_address("localhost", 1214)
 
-print router1.IP
-print router1.PORT
-print router2.IP
-print router2.PORT
-print router3.IP
-print router3.PORT
-print router4.IP
-print router4.PORT
-print router5.IP
-print router5.PORT
+# print router1.IP
+# print router1.PORT
+# print router2.IP
+# print router2.PORT
+# print router3.IP
+# print router3.PORT
+# print router4.IP
+# print router4.PORT
+# print router5.IP
+# print router5.PORT
 
-router1.add_weight()
-router2.add_weight()
-router3.add_weight()
-router4.add_weight()
-router5.add_weight()
+# router1.add_weight()
+# router2.add_weight()
+# router3.add_weight()
+# router4.add_weight()
+# router5.add_weight()
 
-router1.connectNeighbour(router2)
-router2.connectNeighbour(router5)
-router3.connectNeighbour(router4)
+# router1.connectNeighbour(router2)
+# router2.connectNeighbour(router5)
+# router3.connectNeighbour(router4)
 
-print router1.weight
-print router2.weight
-print router3.weight
-print router4.weight
-print router5.weight
+# print router1.weight
+# print router2.weight
+# print router3.weight
+# print router4.weight
+# print router5.weight
