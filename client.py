442b7741f5262a646a984bda00387173c95584b1
Daniel Salami <danialskywalker45@yahoo.com>
2016-05-03 20:30:14 -0500

    added server and client

---
+++ client.py
@@ -0,0 +1,19 @@
+import socket
+
+TCP_IP = '127.0.0.1' 	# address of the server you would like to connect to
+TCP_PORT = 5829		# port you will connect on. THIS SHOULD MATCH THE SERVER FILE
+BUFFER_SIZE = 128	# small buffer size for text
+MESSAGE = ""
+
+print "Welcome to my python network client!"
+
+while(MESSAGE != "exit"):	# loop to keep asking for input unless you get the exit command
+	MESSAGE = raw_input("Enter some text: ")
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # creates socket
+	s.connect((TCP_IP, TCP_PORT))	# connects to the server
+	s.send(MESSAGE)			# sends message
+	data = s.recv(BUFFER_SIZE)	# Recieves return messages
+	s.close()
+	print "reply: ", data
+
+print "Exiting client program!"
