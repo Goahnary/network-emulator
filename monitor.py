b22a2ccb7b729943869e9c8c12cd47e00063323e
Noah Gary <goahnary@gmail.com>
2016-05-04 19:55:08 -0500

    adding graph functionality

--- monitor.py
+++ monitor.py
@@ -1,4 +1,6 @@
-from router import Router 
+from router import Router
+import networkx as nx
+import matplotlib.pyplot as plt
 
 print "() = weight"
 
@@ -16,32 +18,117 @@ r10 = Router(10)
 routers = [r1, r2, r3, r4, r5, r6, r7, r8, r9, r10]
 
 for x in routers:
-	x.add_weight()
+	print "Router " + str(x.routerNum) + ": " + str(x.IP) + " " + str(x.PORT)
 
 
-for x in routers:
-	print "(" + str(x.weight) + ")Hello router " + str(x.routerNum) + ": " + str(x.IP) + " " + str(x.PORT)
+graph = []
 
 # FIXME: FIND RANDOM CONNECTION
 print "connecting r1 to r2: "
+r1.add_neighbor((r2.IP, r2.PORT))
 r1.connectNeighbour(r2)
+graph.append( (1, 2, 5) )
+
 print "connecting r2 to r3: "
+r2.add_neighbor((r3.IP, r3.PORT))
 r2.connectNeighbour(r3)
+graph.append( (2, 3, 2) )
+
 print "connecting r3 to r4: "
+r3.add_neighbor((r4.IP, r4.PORT))
 r3.connectNeighbour(r4)
+graph.append( (3, 4, 9) )
+
 print "connecting r3 to r5: "
+r3.add_neighbor((r5.IP, r5.PORT))
 r3.connectNeighbour(r5)
+graph.append( (3, 5, 1) )
+
 print "connecting r4 to r6: "
+r4.add_neighbor((r6.IP, r6.PORT))
 r4.connectNeighbour(r6)
+graph.append( (4, 6, 1) )
+
 print "connecting r5 to r6: "
+r5.add_neighbor((r6.IP, r6.PORT))
 r5.connectNeighbour(r6)
+graph.append( (5, 6, 10) )
+
 print "connecting r6 to r7: "
+r6.add_neighbor((r7.IP, r7.PORT))
 r6.connectNeighbour(r7)
+graph.append( (6, 7, 7) )
+
 print "connecting r7 to r8: "
+r7.add_neighbor((r8.IP, r8.PORT))
 r7.connectNeighbour(r8)
+graph.append( (7, 8, 1) )
+
 print "connecting r7 to r9: "
+r7.add_neighbor((r9.IP, r9.PORT))
 r7.connectNeighbour(r9)
+graph.append( (7, 9, 8) )
+
 print "connecting r8 to r10: "
+r8.add_neighbor((r10.IP, r10.PORT))
 r8.connectNeighbour(r10)
+graph.append( (8, 10, 11) )
+
 print "connecting r9 to r10: "
+r9.add_neighbor((r10.IP, r10.PORT))
 r9.connectNeighbour(r10)
+graph.append( (9, 10, 2) )
+
+for x in r3.neighbors:
+	print x
+
+
+def draw_graph(graph, labels=None, graph_layout='shell',
+               node_size=1600, node_color='blue', node_alpha=0.3,
+               node_text_size=12,
+               edge_color='blue', edge_alpha=0.3, edge_tickness=1,
+               edge_text_pos=0.3,
+               text_font='sans-serif'):
+
+    # create networkx graph
+    G=nx.Graph()
+
+    # add edges
+    for edge in graph:
+        G.add_edge(edge[0], edge[1], edge[2]) # (edge1, edge2, weight)
+
+    # these are different layouts for the network you may try
+    # shell seems to work best
+    if graph_layout == 'spring':
+        graph_pos=nx.spring_layout(G)
+    elif graph_layout == 'spectral':
+        graph_pos=nx.spectral_layout(G)
+    elif graph_layout == 'random':
+        graph_pos=nx.random_layout(G)
+    else:
+        graph_pos=nx.shell_layout(G)
+
+    # draw graph
+    nx.draw_networkx_nodes(G,graph_pos,node_size=node_size, 
+                           alpha=node_alpha, node_color=node_color)
+    nx.draw_networkx_edges(G,graph_pos,width=edge_tickness,
+                           alpha=edge_alpha,edge_color=edge_color)
+    nx.draw_networkx_labels(G, graph_pos,font_size=node_text_size,
+                            font_family=text_font)
+
+    if labels is None:
+        labels = range(len(graph))
+
+    edge_labels = dict(zip(graph, labels))
+    nx.draw_networkx_edge_labels(G, graph_pos, edge_labels=edge_labels, 
+                                 label_pos=edge_text_pos)
+
+    # show graph
+    plt.show()
+
+G=nx.path_graph(10)
+length,path=nx.single_source_dijkstra(G,1,10)
+
+# print length[10]
+print path[10]
+
--- router.py
+++ router.py
@@ -13,7 +13,7 @@ class Router:
 
 	# CONSTANT VARS
 	BASE_PORT = 7770
-	MULTIPLIER = 7
+	MULTIPLIER = 9
 
 	#TRACKING
 	neighbors = []
@@ -31,24 +31,8 @@ class Router:
 		self.routerNum = rn
 		self.get_address()
 
-	def add_neighbor(self, address): 	# address = (IP, Port)
-		n = add_weight(address)			# returns a 
-		self.neighbors.append(n)
+	def add_neighbor(self, neighbor): 	# address = (IP, Port)
 
-	def add_weight(self): # Adds weight between this router and a neighbor 
-		self.weight = random.randint(1,11)		
-
-	def get_address(self): # Gives router IP and Port
-		self.IP = "localhost"
-		self.PORT = self.BASE_PORT + self.routerNum*self.MULTIPLIER
-
-	def get_neighbors(self): # Returns list of adjacent routers
-		return neighbors
-
-	def update_table(self, json_table): #Updates the forwarding table to the current one
-		self.table = json.loads(json_table)
-
-	def connectNeighbour(self, neighbor):
 		try :
 			self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		except socket.error, msg :
@@ -56,8 +40,28 @@ class Router:
 			sys.exit()
 
 		try:
-			self.socket.bind((neighbor.IP, neighbor.PORT))
+			self.socket.bind((neighbor.IP, self.PORT - len(self.neighbors) ))
+			
+			n = (neighbor.routerNum, neighbor.IP, neighbor.PORT, self.get_weight())			# n = (IP, PORT, WEIGHT, routerNum)
+			self.neighbors.append(n)
+
 			print 'Successfully connected to neighbor.............................'
 		except socket.error, msg :
 			print 'Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
 			sys.exit()
+
+	def get_weight(self): # Adds weight between this router and a neighbor 
+		return random.randint(1,11)
+
+	def get_address(self): # Gives router IP and Port
+		self.IP = "localhost"
+		self.PORT = self.BASE_PORT + self.routerNum*self.MULTIPLIER
+
+	def update_table(self, json_table): #Updates the forwarding table to the current one
+		self.table = json.loads(json_table)
+
+	def connectNeighbour(self, neighbor):#YOU MUST ADD NEIGHBOR BEFORE CONNECTING NEIGHBOR
+		
+
+
+#EOF
